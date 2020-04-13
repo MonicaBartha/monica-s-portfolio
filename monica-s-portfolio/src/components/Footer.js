65ebb26eaa2883df3ce995d3b032ff3fe660b1ca
MonicaBartha <barthamonica@gmail.com>
2020-04-13 10:05:43 -0400

    mobile footer

--- monica-s-portfolio/src/components/Footer.js
+++ monica-s-portfolio/src/components/Footer.js
@@ -11,7 +11,6 @@ function Footer() {
   
     return (
       <div className="divFooter">
-        
           <div className="footerIcons">
             <a href="https://www.linkedin.com/in/monica-bartha/" target="_blank" rel="noopener noreferrer"> 
             <img src={LinkedIn} className="socialIcons" alt="Linkedin" /> </a>
@@ -19,10 +18,21 @@ function Footer() {
             <img src={Github} className="socialIcons" alt="Github" /> </a>
             <a href="https://www.behance.net/monica_bartha" target="_blank" rel="noopener noreferrer">
             <img src={Behance} className="socialIcons" alt="Behance" /> </a>
-            
           </div>
-         
           <VerticalDivider />
+          <div className="mobileFooter">
+            <div className="footerSocialIcons">
+              <a href="https://www.linkedin.com/in/monica-bartha/" target="_blank" rel="noopener noreferrer"> 
+              <img src={LinkedIn} className="footerLinkedInIcon" alt="Linkedin" /> </a>
+              <a href="https://github.com/MonicaBartha" className="footerGitIcon" target="_blank" rel="noopener noreferrer"> 
+              <img src={Github} alt="Github" /> </a>
+              <a href="https://www.behance.net/monica_bartha" className="footerBehanceIcon" target="_blank" rel="noopener noreferrer">
+              <img src={Behance} alt="Behance" /> </a>
+            </div>
+            <div className="emailFooter">
+              <p><a className="emailLink" href="mailto:hello@monicab.dev">hello@monicab.dev</a></p>
+            </div>
+          </div>
       </div>
       
     );
